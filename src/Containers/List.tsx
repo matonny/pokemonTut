a523f5088b318f165dc7337e1f6b21d4232a1345
Mateusz Wygonny <mateusz.wygonny@swmansion.com>
2023-07-04 11:36:29 +0200

    feat: can fetch data when entering list and pressing button

--- src/Containers/List.tsx
+++ src/Containers/List.tsx
@@ -1,14 +1,38 @@
 import { useEffect, useState } from "react";
-import { Text } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export const List = () => {
   const [isLoading, setLoading] = useState(true);
   const [offset, setOffset] = useState(0);
   const pageSize = 25;
 
-  const getPokemons = () => {};
+  const getPokemons = async () => {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/?limit=${pageSize}&offset=${offset}`
+      );
+      const json = await response.json();
+      setOffset((prevOffset) => prevOffset + pageSize);
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     getPokemons();
-  });
-  return <Text>Pokemon List</Text>;
+
+    return () => {
+      setOffset(0);
+      setLoading(true);
+    };
+  }, []);
+  return (
+    <View>
+      <Text>Pokemon List</Text>
+      <Button onPress={getPokemons} title="Fetch more"></Button>
+    </View>
+  );
 };
